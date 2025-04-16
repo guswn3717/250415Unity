dbd1e0ce65b2a7c8efc4d080b5d9fcc73a06a371
guswn3717 <rudals9973@gmail.com>
2025-04-17 08:55:34 +0900

    20250416

--- Game/Assets/Class3th (Life Cycle)/Scripts/State.cs
+++ Game/Assets/Class3th (Life Cycle)/Scripts/State.cs
@@ -30,27 +30,43 @@ public class State : MonoBehaviour
         Debug.Log("Start");
     }
 
+    private void FixedUpdate()
+    {
+        // FixedUpdate 함수란?
+        // TimeStep에 설정된 값에 따라 일정한 간격으로 호출되는 이벤트 함수입니다.
+
+        Debug.Log("Fixed Update");
+    }
+
     private void Update()
     {
-        //매 프레임마다 호출되는 함수입니다
+        // Update 함수란?
+        // 게임이 실행되는 동안 매 프레임마다 호출되는 이벤트 함수입니다.
+
         Debug.Log("Update");
     }
 
     private void LateUpdate()
     {
-        //Update가 실행된 후 실행되는 함수입니다
+        // Late Update
+        // Update 함수가 호출된 이후에 호출되는 이벤트 함수입니다.
+
         Debug.Log("Late Update");
     }
 
     private void OnDisable()
     {
-        //게임 오브젝트가 비활성화 되었을 때 호출되는 이벤트 함수입니다
+        // OnDisable
+        // 게임 오브젝트가 비활성화 되었을 때 호출되는 이벤트 함수입니다.
+
         Debug.Log("OnDisable");
     }
 
     private void OnDestroy()
     {
-        //게임 오브젝트가 삭제되었을 때 호출되는 이벤트 함수입니다.
+        // OnDestroy
+        // 게임 오브젝트가 삭제되었을 때 호출되는 이벤트 함수입니다.
+
         Debug.Log("OnDestroy");
     }
 }
