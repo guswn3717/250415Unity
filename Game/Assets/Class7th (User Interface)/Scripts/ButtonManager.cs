f141f9ddf4ba937948ed46ac356e5c9669081914
guswn3717 <rudals9973@gmail.com>
2025-04-21 12:00:00 +0900

    4월 21일 날짜로 된 커밋 메시지

---
+++ Game/Assets/Class7th (User Interface)/Scripts/ButtonManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] Text [ ] textList;
+    [SerializeField] string [ ] titleList;
+
+    private void Start()
+    {
+        for(int i = 0; i < textList.Length; i++)
+        {
+            textList[i].text = titleList[i];
+        }
+    }
+
+    public void Execute()
+    {
+        Debug.Log("Execute");
+    }
+
+    public void Shop()
+    {
+        Debug.Log("Shop");
+    }
+    
+    public void Quit()
+    {
+        Debug.Log("Quit");
+    }
+}
